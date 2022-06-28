1059b148ebdf364a63e6e0b62c8ab81892784871
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-06-28 23:26:19 +0300

    Logging data with fetch.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const get_data_from_url = async ( url ) => {
+    const data = await fetch('https://swapi.dev/api/people/1');
+    const parsed_data = await data.json();
+    console.log( parsed_data.name );
+  };
+  get_data_from_url();
+
   return (
     <div className="App">
       App
