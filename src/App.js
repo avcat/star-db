46b4b847f5c02ef489b371934fa96f455ea19ded
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-06-28 23:42:08 +0300

    Added some error logging while trying to fetch.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+const App = () => {
+
   const get_data_from_url = async ( url ) => {
-    const data = await fetch('https://swapi.dev/api/people/1');
-    const parsed_data = await data.json();
-    console.log( parsed_data.name );
-  };
-  get_data_from_url();
+    try {
+      const response = await fetch( url );
+      if ( !response.ok ) { // if result status is not the one of 200-299
+        throw new Error( `Could not fetch from ${ url } - received status ${ response.status }` );
+      }
+
+      const data = await response.json();
+      console.log( data.name );
+    }
+    catch( err ) {
+      console.error( `No way! ${err}` );
+    };
+  }
+  get_data_from_url( 'https://swapi.dev/api/people/123432' );
 
   return (
     <div className="App">
