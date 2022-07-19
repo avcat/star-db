7a831afcc0324bf9d681913ac51e71c0b7e2e2c5
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-07-19 13:37:23 +0200

    Conditiobal spinner component while loading, private Planet component to separate the content from the state, using React.Fragments.

--- src/components/Planet/Planet.js
+++ src/components/Planet/Planet.js
@@ -7,7 +7,8 @@ import Spinner from '../Spinner';
 export default class Planet extends React.Component {
 
 	state = {
-		planet: {}
+		planet: {},
+		loading: true
 	}
 
 	constructor(props) {
@@ -20,49 +21,66 @@ export default class Planet extends React.Component {
 
 	get_data = async (id) => {
 		const planet = await this.SwapiService.get_single_planet(id);
-		this.setState({planet});
+		this.setState({
+			planet,
+			loading: false
+		});
 	}
 
 	render() {
 
 		const {
-			name,
-			population,
-			rotation_period,
-			diameter,
-			image_url
-		} = this.state.planet;
+			planet,
+			loading
+		} = this.state;
 
-		const image_html = image_url ? (
-			<img className="image" src={image_url} alt='details' />
-		) : ( null );
+		const content = loading ?
+			<Spinner width={60} /> :
+			<PlanetContent planet={planet} />;
 
 		return (
-			<div className='Planet card d-flex flex-row align-items-center p-4 gap-3'>
-
-				{image_html}
-
-				<Spinner width={60} />
-
-				<div className="card-body">
-					<h4 className='text-center'>{name}</h4>
-
-					<ul className="list-group">
-						<li className="list-group-item d-flex justify-content-between">
-							<span className="term">Population</span>
-							<span>{population}</span>
-						</li>
-						<li className="list-group-item d-flex justify-content-between">
-							<span className="term">Rotation period</span>
-							<span>{rotation_period}</span>
-						</li>
-						<li className="list-group-item d-flex justify-content-between">
-							<span className="term">Diameter</span>
-							<span>{diameter}</span>
-						</li>
-					</ul>
-				</div>
+			<div className='Planet card d-flex flex-row align-items-center p-4 gap-3 justify-content-center'>
+				{content}
 			</div>
 		);
 	}
+}
+
+const PlanetContent = ({planet}) => {
+	const {
+		name,
+		population,
+		rotation_period,
+		diameter,
+		image_url
+	} = planet;
+
+	const image_html = image_url ? (
+		<img className="image rounded" src={image_url} alt='details' />
+	) : ( null );
+
+	return (
+		<React.Fragment>
+			{image_html}
+
+			<div className="card-body">
+				<h4 className='text-center'>{name}</h4>
+
+				<ul className="list-group">
+					<li className="list-group-item d-flex justify-content-between">
+						<span className="term">Population</span>
+						<span>{population}</span>
+					</li>
+					<li className="list-group-item d-flex justify-content-between">
+						<span className="term">Rotation period</span>
+						<span>{rotation_period}</span>
+					</li>
+					<li className="list-group-item d-flex justify-content-between">
+						<span className="term">Diameter</span>
+						<span>{diameter}</span>
+					</li>
+				</ul>
+			</div>
+		</React.Fragment>
+	);
 }
