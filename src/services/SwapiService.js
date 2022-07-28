c7f9d788e29274de9d3041d6fed77f2e4843b849
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-07-28 18:56:35 +0200

    Removed not needed elements. Changed swapi async functions to arrow functions to make them keep their context.

--- src/services/SwapiService.js
+++ src/services/SwapiService.js
@@ -1,4 +1,4 @@
-import { get_properties } from './helpers.js';
+import {get_properties} from './helpers.js';
 
 export default class Swapi_Service {
 
@@ -9,41 +9,41 @@ export default class Swapi_Service {
 	_api_base = 'https://swapi.dev/api';
 	_api_img_base = 'https://starwars-visualguide.com/assets/img';
 
-	async get_data_from_url ( url_part, type = 'data' ) {
+	get_data_from_url = async (url_part, type = 'data') => {
 
 		try {
 			const full_path = type === 'data' ? (
-				`${ this._api_base }${ url_part }`
+				`${this._api_base}${url_part}`
 			) : (
-				`${ this._api_img_base }${ url_part }`
+				`${this._api_img_base}${url_part}`
 			);
 
-			const response = await fetch( full_path );
-			if ( !response.ok ) { // if result status is not the one of 200-299
-				throw new Error( `Could not fetch from ${ full_path } - received status ${ response.status }` );
+			const response = await fetch(full_path);
+			if (!response.ok) {// if result status is not the one of 200-299
+				throw new Error(`Could not fetch from ${full_path} - received status ${response.status}`);
 			}
 			const data = type === 'data' ? await response.json() : await response.url;
 			return data;
 		}
-		catch( err ) {
+		catch(err) {
 			// TODO: handle catch for absent images differently
-			console.error( `No way! ${err}` );
+			console.error(`No way! ${err}`);
 		};
 	}
 
-	get_id = ( object ) => {
+	get_id = (object) => {
 		const id = parseInt(object?.url.match(/\d+/g)[0]);
 		return id;
 	}
 
 	// -------------------------------- PEOPLE --------------------------------
 
-	async get_person_image( id ) {
-		const single_img = await this.get_data_from_url( `/characters/${ id }.jpg`, 'img' );
+	get_person_image = async (id) => {
+		const single_img = await this.get_data_from_url(`/characters/${id}.jpg`, 'img');
 		return single_img;
 	}
 
-	async transform_data_person(person) {
+	transform_data_person = async (person) => {
 		const id = this.get_id(person);
 		const image_url = await this.get_person_image(id);
 
@@ -57,14 +57,14 @@ export default class Swapi_Service {
 		}
 	}
 
-	async get_single_person( id ) {
-		const person = await this.get_data_from_url( `/people/${ id }` );
+	get_single_person = async (id) => {
+		const person = await this.get_data_from_url(`/people/${id}`);
 		const transformed_data = await this.transform_data_person(person);
 		return transformed_data;
 	}
 
-	async get_all_people(page = 1) {
-		const data = await this.get_data_from_url( `/people/?page=${page}` );
+	get_all_people = async (page = 1) => {
+		const data = await this.get_data_from_url(`/people/?page=${page}`);
 		const transformed_data = await data.results.map(async (person) => {
 			const transformed_data = await this.transform_data_person(person);
 			return transformed_data;
@@ -75,12 +75,12 @@ export default class Swapi_Service {
 
 	// -------------------------------- PLANETS --------------------------------
 
-	async get_planet_image( id ) {
-		const single_img = await this.get_data_from_url( `/planets/${ id }.jpg`, 'img' );
+	get_planet_image = async (id) => {
+		const single_img = await this.get_data_from_url(`/planets/${id}.jpg`, 'img');
 		return single_img;
 	}
 
-	async transform_data_planet(planet) {
+	transform_data_planet = async (planet) => {
 		const id = this.get_id(planet);
 		const image_url = await this.get_planet_image(id);
 
@@ -94,14 +94,14 @@ export default class Swapi_Service {
 		}
 	}
 
-	async get_single_planet( id ) {
-		const planet = await this.get_data_from_url( `/planets/${ id }` );
+	get_single_planet = async (id) => {
+		const planet = await this.get_data_from_url(`/planets/${id}`);
 		const transformed_data = await this.transform_data_planet(planet);
 		return transformed_data;
 	}
 
-	async get_all_planets(page = 1) {
-		const data = await this.get_data_from_url( `/planets/?page=${page}` );
+	get_all_planets = async (page = 1) => {
+		const data = await this.get_data_from_url(`/planets/?page=${page}`);
 		const transformed_data = await data.results.map(async (planet) => {
 			const transformed_data = await this.transform_data_planet(planet);
 			return transformed_data;
@@ -112,12 +112,12 @@ export default class Swapi_Service {
 
 	// -------------------------------- STARSHIPS --------------------------------
 
-	async get_starship_image( id ) {
-		const single_img = await this.get_data_from_url( `/starships/${ id }.jpg`, 'img' );
+	get_starship_image = async (id) => {
+		const single_img = await this.get_data_from_url(`/starships/${id}.jpg`, 'img');
 		return single_img;
 	}
 
-	async transform_data_starship(starship) {
+	transform_data_starship = async (starship) => {
 		const id = this.get_id(starship);
 		const image_url = await this.get_starship_image(id);
 
@@ -135,14 +135,14 @@ export default class Swapi_Service {
 		}
 	}
 
-	async get_single_starship( id ) {
-		const starship = await this.get_data_from_url( `/starships/${ id }` );
+	get_single_starship = async (id) => {
+		const starship = await this.get_data_from_url(`/starships/${id}`);
 		const transformed_data = await this.transform_data_starship(starship);
 		return transformed_data;
 	}
 
-	async get_all_starships(page = 1) {
-		const data = await this.get_data_from_url( `/starships/?page=${page}` );
+	get_all_starships = async (page = 1) => {
+		const data = await this.get_data_from_url(`/starships/?page=${page}`);
 		const transformed_data = await data.results.map(async (starship) => {
 			const transformed_data = await this.transform_data_starship(starship);
 			return transformed_data;
